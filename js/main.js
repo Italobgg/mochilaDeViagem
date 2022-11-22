9cef283147df215a20b8696006c19a3b3bc6f77f
italo <italobartolomeugarcia@gmail.com>
2022-11-22 19:27:57 -0300

    Agora pode atualizar itens

--- js/main.js
+++ js/main.js
@@ -1,41 +1,58 @@
-const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach((elemento) => {
-  criaElemento(elemento);
-});
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
 
 form.addEventListener("submit", (evento) => {
-  evento.preventDefault();
+    evento.preventDefault()
 
-  const nome = evento.target.elements["nome"];
-  const quantidade = evento.target.elements["quantidade"];
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
-  const itemAtual = {
-    nome: nome.value,
-    quantidade: quantidade.value,
-  };
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
-  criaElemento(itemAtual);
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
 
-  itens.push(itemAtual);
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
 
-  localStorage.setItem("itens", JSON.stringify(itens));
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
 
-  nome.value = "";
-  quantidade.value = "";
-});
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
 
 function criaElemento(item) {
-  const novoItem = document.createElement("li");
-  novoItem.classList.add("item");
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
 
-  const numeroItem = document.createElement("strong");
-  numeroItem.innerHTML = item.quantidade;
-  novoItem.appendChild(numeroItem);
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
+    
+    novoItem.innerHTML += item.nome
 
-  novoItem.innerHTML += item.nome;
+    lista.appendChild(novoItem)
+}
 
-  lista.appendChild(novoItem);
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
