66487fe666a04420156bec0b43f3f0127a82f4bc
italo <italobartolomeugarcia@gmail.com>
2022-11-22 13:39:56 -0300

    salvado dados em localStorage e exibindo na interface

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = []
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
 
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+  evento.preventDefault();
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
 
-    criaElemento(nome.value, quantidade.value)
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
 
-    nome.value = ""
-    quantidade.value = ""
-})
+  criaElemento(itemAtual);
 
-function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+  itens.push(itemAtual);
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-    novoItem.appendChild(numeroItem)
+  localStorage.setItem("itens", JSON.stringify(itens));
 
-    novoItem.innerHTML += nome
+  nome.value = "";
+  quantidade.value = "";
+});
 
-    lista.appendChild(novoItem)
+function criaElemento(item) {
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
 
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = item.quantidade;
+  novoItem.appendChild(numeroItem);
 
-    itens.push(itemAtual)
+  novoItem.innerHTML += item.nome;
 
-    localStorage.setItem("item", JSON.stringify(itens))
+  lista.appendChild(novoItem);
 }
